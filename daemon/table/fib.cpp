ae19a84d9297b5091f87e5db7c1de331d0922d34
Shohei Okubo <s.o.w.u@ruri.waseda.jp>
2021-04-27 21:25:50 +0900

    format

--- daemon/table/fib.cpp
+++ daemon/table/fib.cpp
@@ -3,31 +3,32 @@
  * Copyright (c) 2014-2019,  Regents of the University of California,
  *                           Arizona Board of Regents,
  *                           Colorado State University,
- *                           University Pierre & Marie Curie, Sorbonne University,
- *                           Washington University in St. Louis,
- *                           Beijing Institute of Technology,
- *                           The University of Memphis.
+ *                           University Pierre & Marie Curie, Sorbonne
+ * University, Washington University in St. Louis, Beijing Institute of
+ * Technology, The University of Memphis.
  *
  * This file is part of NFD (Named Data Networking Forwarding Daemon).
  * See AUTHORS.md for complete list of NFD authors and contributors.
  *
- * NFD is free software: you can redistribute it and/or modify it under the terms
- * of the GNU General Public License as published by the Free Software Foundation,
- * either version 3 of the License, or (at your option) any later version.
+ * NFD is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
  *
- * NFD is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
- * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
- * PURPOSE.  See the GNU General Public License for more details.
+ * NFD is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License along with
  * NFD, e.g., in COPYING.md file.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "fib.hpp"
-#include "pit-entry.hpp"
-#include "measurements-entry.hpp"
 
 #include <ndn-cxx/util/concepts.hpp>
+
+#include "measurements-entry.hpp"
+#include "pit-entry.hpp"
 
 namespace nfd {
 namespace fib {
@@ -36,62 +37,46 @@ NDN_CXX_ASSERT_FORWARD_ITERATOR(Fib::const_iterator);
 
 const unique_ptr<Entry> Fib::s_emptyEntry = make_unique<Entry>(Name());
 
-static inline bool
-nteHasFibEntry(const name_tree::Entry& nte)
-{
+static inline bool nteHasFibEntry(const name_tree::Entry& nte) {
   return nte.getFibEntry() != nullptr;
 }
 
-Fib::Fib(NameTree& nameTree)
-  : m_nameTree(nameTree)
-{
-}
+Fib::Fib(NameTree& nameTree) : m_nameTree(nameTree) {}
 
-template<typename K>
-const Entry&
-Fib::findLongestPrefixMatchImpl(const K& key) const
-{
-  name_tree::Entry* nte = m_nameTree.findLongestPrefixMatch(key, &nteHasFibEntry);
-  if (nte != nullptr) {
+template <typename K>
+const Entry& Fib::findLongestPrefixMatchImpl(const K& key) const {
+  name_tree::Entry* nte =
+      m_nameTree.findLongestPrefixMatch(key, &nteHasFibEntry);
+  if(nte != nullptr) {
     return *nte->getFibEntry();
   }
   return *s_emptyEntry;
 }
 
-const Entry&
-Fib::findLongestPrefixMatch(const Name& prefix) const
-{
+const Entry& Fib::findLongestPrefixMatch(const Name& prefix) const {
   return this->findLongestPrefixMatchImpl(prefix);
 }
 
-const Entry&
-Fib::findLongestPrefixMatch(const pit::Entry& pitEntry) const
-{
+const Entry& Fib::findLongestPrefixMatch(const pit::Entry& pitEntry) const {
   return this->findLongestPrefixMatchImpl(pitEntry);
 }
 
-const Entry&
-Fib::findLongestPrefixMatch(const measurements::Entry& measurementsEntry) const
-{
+const Entry& Fib::findLongestPrefixMatch(
+    const measurements::Entry& measurementsEntry) const {
   return this->findLongestPrefixMatchImpl(measurementsEntry);
 }
 
-Entry*
-Fib::findExactMatch(const Name& prefix)
-{
+Entry* Fib::findExactMatch(const Name& prefix) {
   name_tree::Entry* nte = m_nameTree.findExactMatch(prefix);
-  if (nte != nullptr)
-    return nte->getFibEntry();
+  if(nte != nullptr) return nte->getFibEntry();
 
   return nullptr;
 }
 
-std::pair<Entry*, bool>
-Fib::insert(const Name& prefix)
-{
+std::pair<Entry*, bool> Fib::insert(const Name& prefix) {
   name_tree::Entry& nte = m_nameTree.lookup(prefix);
   Entry* entry = nte.getFibEntry();
-  if (entry != nullptr) {
+  if(entry != nullptr) {
     return {entry, false};
   }
 
@@ -100,73 +85,59 @@ Fib::insert(const Name& prefix)
   return {nte.getFibEntry(), true};
 }
 
-void
-Fib::erase(name_tree::Entry* nte, bool canDeleteNte)
-{
+void Fib::erase(name_tree::Entry* nte, bool canDeleteNte) {
   BOOST_ASSERT(nte != nullptr);
 
   nte->setFibEntry(nullptr);
-  if (canDeleteNte) {
+  if(canDeleteNte) {
     m_nameTree.eraseIfEmpty(nte);
   }
   --m_nItems;
 }
 
-void
-Fib::erase(const Name& prefix)
-{
+void Fib::erase(const Name& prefix) {
   name_tree::Entry* nte = m_nameTree.findExactMatch(prefix);
-  if (nte != nullptr) {
+  if(nte != nullptr) {
     this->erase(nte);
   }
 }
 
-void
-Fib::erase(const Entry& entry)
-{
+void Fib::erase(const Entry& entry) {
   name_tree::Entry* nte = m_nameTree.getEntry(entry);
-  if (nte == nullptr) { // don't try to erase s_emptyEntry
+  if(nte == nullptr) {  // don't try to erase s_emptyEntry
     BOOST_ASSERT(&entry == s_emptyEntry.get());
     return;
   }
   this->erase(nte);
 }
 
-void
-Fib::addOrUpdateNextHop(Entry& entry, Face& face, uint64_t cost)
-{
+void Fib::addOrUpdateNextHop(Entry& entry, Face& face, uint64_t cost) {
   NextHopList::iterator it;
   bool isNew;
   std::tie(it, isNew) = entry.addOrUpdateNextHop(face, cost);
 
-  if (isNew)
-    this->afterNewNextHop(entry.getPrefix(), *it);
+  if(isNew) this->afterNewNextHop(entry.getPrefix(), *it);
 }
 
-Fib::RemoveNextHopResult
-Fib::removeNextHop(Entry& entry, const Face& face)
-{
+Fib::RemoveNextHopResult Fib::removeNextHop(Entry& entry, const Face& face) {
   bool isRemoved = entry.removeNextHop(face);
 
-  if (!isRemoved) {
+  if(!isRemoved) {
     return RemoveNextHopResult::NO_SUCH_NEXTHOP;
-  }
-  else if (!entry.hasNextHops()) {
+  } else if(!entry.hasNextHops()) {
     name_tree::Entry* nte = m_nameTree.getEntry(entry);
     this->erase(nte, false);
     return RemoveNextHopResult::FIB_ENTRY_REMOVED;
-  }
-  else {
+  } else {
     return RemoveNextHopResult::NEXTHOP_REMOVED;
   }
 }
 
-Fib::Range
-Fib::getRange() const
-{
+Fib::Range Fib::getRange() const {
   return m_nameTree.fullEnumerate(&nteHasFibEntry) |
-         boost::adaptors::transformed(name_tree::GetTableEntry<Entry>(&name_tree::Entry::getFibEntry));
+         boost::adaptors::transformed(
+             name_tree::GetTableEntry<Entry>(&name_tree::Entry::getFibEntry));
 }
 
-} // namespace fib
-} // namespace nfd
+}  // namespace fib
+}  // namespace nfd
--- daemon/table/name-tree.cpp
+++ daemon/table/name-tree.cpp
@@ -3,46 +3,42 @@
  * Copyright (c) 2014-2019,  Regents of the University of California,
  *                           Arizona Board of Regents,
  *                           Colorado State University,
- *                           University Pierre & Marie Curie, Sorbonne University,
- *                           Washington University in St. Louis,
- *                           Beijing Institute of Technology,
- *                           The University of Memphis.
+ *                           University Pierre & Marie Curie, Sorbonne
+ * University, Washington University in St. Louis, Beijing Institute of
+ * Technology, The University of Memphis.
  *
  * This file is part of NFD (Named Data Networking Forwarding Daemon).
  * See AUTHORS.md for complete list of NFD authors and contributors.
  *
- * NFD is free software: you can redistribute it and/or modify it under the terms
- * of the GNU General Public License as published by the Free Software Foundation,
- * either version 3 of the License, or (at your option) any later version.
+ * NFD is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
  *
- * NFD is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
- * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
- * PURPOSE.  See the GNU General Public License for more details.
+ * NFD is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License along with
  * NFD, e.g., in COPYING.md file.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "name-tree.hpp"
-#include "common/logger.hpp"
 
 #include <boost/concept/assert.hpp>
 #include <boost/concept_check.hpp>
 #include <type_traits>
+
+#include "common/logger.hpp"
 
 namespace nfd {
 namespace name_tree {
 
 NFD_LOG_INIT(NameTree);
 
-NameTree::NameTree(size_t nBuckets)
-  : m_ht(HashtableOptions(nBuckets))
-{
-}
+NameTree::NameTree(size_t nBuckets) : m_ht(HashtableOptions(nBuckets)) {}
 
-Entry&
-NameTree::lookup(const Name& name, size_t prefixLen)
-{
+Entry& NameTree::lookup(const Name& name, size_t prefixLen) {
   NFD_LOG_TRACE("lookup(" << name << ", " << prefixLen << ')');
   BOOST_ASSERT(prefixLen <= name.size());
   BOOST_ASSERT(prefixLen <= getMaxDepth());
@@ -51,11 +47,11 @@ NameTree::lookup(const Name& name, size_t prefixLen)
   const Node* node = nullptr;
   Entry* parent = nullptr;
 
-  for (size_t i = 0; i <= prefixLen; ++i) {
+  for(size_t i = 0; i <= prefixLen; ++i) {
     bool isNew = false;
     std::tie(node, isNew) = m_ht.insert(name, i, hashes);
 
-    if (isNew && parent != nullptr) {
+    if(isNew && parent != nullptr) {
       node->entry.setParent(*parent);
     }
     parent = &node->entry;
@@ -63,12 +59,10 @@ NameTree::lookup(const Name& name, size_t prefixLen)
   return node->entry;
 }
 
-Entry&
-NameTree::lookup(const fib::Entry& fibEntry)
-{
+Entry& NameTree::lookup(const fib::Entry& fibEntry) {
   NFD_LOG_TRACE("lookup(FIB " << fibEntry.getPrefix() << ')');
   Entry* nte = this->getEntry(fibEntry);
-  if (nte == nullptr) {
+  if(nte == nullptr) {
     // special case: Fib::s_emptyEntry is unattached
     BOOST_ASSERT(fibEntry.getPrefix().empty());
     return this->lookup(fibEntry.getPrefix());
@@ -78,28 +72,25 @@ NameTree::lookup(const fib::Entry& fibEntry)
   return *nte;
 }
 
-Entry&
-NameTree::lookup(const pit::Entry& pitEntry)
-{
+Entry& NameTree::lookup(const pit::Entry& pitEntry) {
   const Name& name = pitEntry.getName();
   NFD_LOG_TRACE("lookup(PIT " << name << ')');
   bool hasDigest = name.size() > 0 && name[-1].isImplicitSha256Digest();
-  if (hasDigest && name.size() <= getMaxDepth()) {
+  if(hasDigest && name.size() <= getMaxDepth()) {
     return this->lookup(name);
   }
 
   Entry* nte = this->getEntry(pitEntry);
   BOOST_ASSERT(nte != nullptr);
-  BOOST_ASSERT(std::count_if(nte->getPitEntries().begin(), nte->getPitEntries().end(),
-                             [&pitEntry] (const auto& pitEntry1) {
+  BOOST_ASSERT(std::count_if(nte->getPitEntries().begin(),
+                             nte->getPitEntries().end(),
+                             [&pitEntry](const auto& pitEntry1) {
                                return pitEntry1.get() == &pitEntry;
                              }) == 1);
   return *nte;
 }
 
-Entry&
-NameTree::lookup(const measurements::Entry& measurementsEntry)
-{
+Entry& NameTree::lookup(const measurements::Entry& measurementsEntry) {
   NFD_LOG_TRACE("lookup(M " << measurementsEntry.getName() << ')');
   Entry* nte = this->getEntry(measurementsEntry);
   BOOST_ASSERT(nte != nullptr);
@@ -108,9 +99,7 @@ NameTree::lookup(const measurements::Entry& measurementsEntry)
   return *nte;
 }
 
-Entry&
-NameTree::lookup(const strategy_choice::Entry& strategyChoiceEntry)
-{
+Entry& NameTree::lookup(const strategy_choice::Entry& strategyChoiceEntry) {
   NFD_LOG_TRACE("lookup(SC " << strategyChoiceEntry.getPrefix() << ')');
   Entry* nte = this->getEntry(strategyChoiceEntry);
   BOOST_ASSERT(nte != nullptr);
@@ -119,38 +108,35 @@ NameTree::lookup(const strategy_choice::Entry& strategyChoiceEntry)
   return *nte;
 }
 
-size_t
-NameTree::eraseIfEmpty(Entry* entry, bool canEraseAncestors)
-{
+size_t NameTree::eraseIfEmpty(Entry* entry, bool canEraseAncestors) {
   BOOST_ASSERT(entry != nullptr);
 
   size_t nErased = 0;
-  for (Entry* parent = nullptr; entry != nullptr && entry->isEmpty(); entry = parent) {
+  for(Entry* parent = nullptr; entry != nullptr && entry->isEmpty();
+      entry = parent) {
     parent = entry->getParent();
 
-    if (parent != nullptr) {
+    if(parent != nullptr) {
       entry->unsetParent();
     }
 
     m_ht.erase(getNode(*entry));
     ++nErased;
 
-    if (!canEraseAncestors) {
+    if(!canEraseAncestors) {
       break;
     }
   }
 
-  if (nErased == 0) {
+  if(nErased == 0) {
     NFD_LOG_TRACE("not-erase " << entry->getName());
   }
   return nErased;
 }
 
-Entry*
-NameTree::findExactMatch(const Name& name, size_t prefixLen) const
-{
+Entry* NameTree::findExactMatch(const Name& name, size_t prefixLen) const {
   prefixLen = std::min(name.size(), prefixLen);
-  if (prefixLen > getMaxDepth()) {
+  if(prefixLen > getMaxDepth()) {
     return nullptr;
   }
 
@@ -158,22 +144,19 @@ NameTree::findExactMatch(const Name& name, size_t prefixLen) const
   return node == nullptr ? nullptr : &node->entry;
 }
 
-Entry*
-NameTree::findExactIDMatch(const Name& name) const
-{
+Entry* NameTree::findExactIDMatch(const Name& name) const {
   const Node* node = m_ht.findByID(name);
   return node == nullptr ? nullptr : &node->entry;
 }
 
-Entry*
-NameTree::findLongestPrefixMatch(const Name& name, const EntrySelector& entrySelector) const
-{
+Entry* NameTree::findLongestPrefixMatch(
+    const Name& name, const EntrySelector& entrySelector) const {
   size_t depth = std::min(name.size(), getMaxDepth());
   HashSequence hashes = computeHashes(name, depth);
 
-  for (ssize_t i = depth; i >= 0; --i) {
+  for(ssize_t i = depth; i >= 0; --i) {
     const Node* node = m_ht.find(name, i, hashes);
-    if (node != nullptr && entrySelector(node->entry)) {
+    if(node != nullptr && entrySelector(node->entry)) {
       return &node->entry;
     }
   }
@@ -181,12 +164,11 @@ NameTree::findLongestPrefixMatch(const Name& name, const EntrySelector& entrySel
   return nullptr;
 }
 
-Entry*
-NameTree::findLongestPrefixMatch(const Entry& entry1, const EntrySelector& entrySelector) const
-{
+Entry* NameTree::findLongestPrefixMatch(
+    const Entry& entry1, const EntrySelector& entrySelector) const {
   Entry* entry = const_cast<Entry*>(&entry1);
-  while (entry != nullptr) {
-    if (entrySelector(*entry)) {
+  while(entry != nullptr) {
+    if(entrySelector(*entry)) {
       return entry;
     }
     entry = entry->getParent();
@@ -194,19 +176,19 @@ NameTree::findLongestPrefixMatch(const Entry& entry1, const EntrySelector& entry
   return nullptr;
 }
 
-Entry*
-NameTree::findLongestPrefixMatch(const pit::Entry& pitEntry, const EntrySelector& entrySelector) const
-{
+Entry* NameTree::findLongestPrefixMatch(
+    const pit::Entry& pitEntry, const EntrySelector& entrySelector) const {
   const Entry* nte = this->getEntry(pitEntry);
   BOOST_ASSERT(nte != nullptr);
 
   const Name& name = pitEntry.getName();
   size_t depth = std::min(name.size(), getMaxDepth());
-  if (nte->getName().size() < pitEntry.getName().size()) {
-    // PIT entry name either exceeds depth limit or ends with an implicit digest: go deeper
-    for (size_t i = nte->getName().size() + 1; i <= depth; ++i) {
+  if(nte->getName().size() < pitEntry.getName().size()) {
+    // PIT entry name either exceeds depth limit or ends with an implicit
+    // digest: go deeper
+    for(size_t i = nte->getName().size() + 1; i <= depth; ++i) {
       const Entry* exact = this->findExactMatch(name, i);
-      if (exact == nullptr) {
+      if(exact == nullptr) {
         break;
       }
       nte = exact;
@@ -216,9 +198,8 @@ NameTree::findLongestPrefixMatch(const pit::Entry& pitEntry, const EntrySelector
   return this->findLongestPrefixMatch(*nte, entrySelector);
 }
 
-boost::iterator_range<NameTree::const_iterator>
-NameTree::findAllMatches(const Name& name, const EntrySelector& entrySelector) const
-{
+boost::iterator_range<NameTree::const_iterator> NameTree::findAllMatches(
+    const Name& name, const EntrySelector& entrySelector) const {
   // As we are using Name Prefix Hash Table, and the current LPM() is
   // implemented as starting from full name, and reduce the number of
   // components by 1 each time, we could use it here.
@@ -226,22 +207,26 @@ NameTree::findAllMatches(const Name& name, const EntrySelector& entrySelector) c
   // trie from the root node.
 
   Entry* entry = this->findLongestPrefixMatch(name, entrySelector);
-  return {Iterator(make_shared<PrefixMatchImpl>(*this, entrySelector), entry), end()};
+  return {Iterator(make_shared<PrefixMatchImpl>(*this, entrySelector), entry),
+          end()};
 }
 
-boost::iterator_range<NameTree::const_iterator>
-NameTree::fullEnumerate(const EntrySelector& entrySelector) const
-{
-  return {Iterator(make_shared<FullEnumerationImpl>(*this, entrySelector), nullptr), end()};
+boost::iterator_range<NameTree::const_iterator> NameTree::fullEnumerate(
+    const EntrySelector& entrySelector) const {
+  return {
+      Iterator(make_shared<FullEnumerationImpl>(*this, entrySelector), nullptr),
+      end()};
 }
 
-boost::iterator_range<NameTree::const_iterator>
-NameTree::partialEnumerate(const Name& prefix,
-                           const EntrySubTreeSelector& entrySubTreeSelector) const
-{
+boost::iterator_range<NameTree::const_iterator> NameTree::partialEnumerate(
+    const Name& prefix,
+    const EntrySubTreeSelector& entrySubTreeSelector) const {
   Entry* entry = this->findExactMatch(prefix);
-  return {Iterator(make_shared<PartialEnumerationImpl>(*this, entrySubTreeSelector), entry), end()};
+  return {
+      Iterator(make_shared<PartialEnumerationImpl>(*this, entrySubTreeSelector),
+               entry),
+      end()};
 }
 
-} // namespace name_tree
-} // namespace nfd
+}  // namespace name_tree
+}  // namespace nfd
